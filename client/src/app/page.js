7968d81afbce30c62a88063ade9bf3f181f0b411
MOHAMMED SALMAN <mack000000sheikh@gmail.com>
2025-05-08 15:37:43 +0000

    updated Name

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Vehicle Management System</h1>
+      <h1 className="text-4xl font-bold mb-8">Welcome To The Task Management System</h1>
       <div className="flex gap-4">
         <Button asChild>
           <Link href="/login">Login</Link>
