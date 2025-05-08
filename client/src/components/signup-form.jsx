92bf1b627ca9db886ab5f945736d7a9312f0d44d
MOHAMMED SALMAN <mack000000sheikh@gmail.com>
2025-05-08 16:34:36 +0000

    corrected css of the sign up page

--- client/src/components/signup-form.jsx
+++ client/src/components/signup-form.jsx
@@ -74,8 +74,8 @@ const SignupForm = () => {
   };
 
   return (
-    
-      <Card className="relative z-10 items-center justify-center w-full max-w-md ">
+    <div className="flex items-center justify-center min-h-screen">
+      <Card className=" justify-center w-full max-w-md ">
         <CardHeader>
           <CardTitle className="text-center  text-2xl font-bold">
             Welcome To Our Platform
@@ -206,6 +206,7 @@ const SignupForm = () => {
           </p>
         </CardFooter>
       </Card>
+      </div>
   );
 };
 
